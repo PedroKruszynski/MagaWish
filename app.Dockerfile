7c2cac87c99546fb93a3dbb311aa608523544c47
PedroKruszynski <pedrokruszynski@gmail.com>
2025-03-10 11:43:56 -0300

    feat: docker working

---
+++ app.Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.10
+
+WORKDIR /app
+
+COPY --from=ghcr.io/astral-sh/uv:0.5.11 /uv /uvx /bin/
+
+ENV PATH="/app/.venv/bin:$PATH"
+
+ENV UV_COMPILE_BYTECODE=1
+
+ENV UV_LINK_MODE=copy
+
+COPY pyproject.toml alembic.ini /app/
+
+RUN --mount=type=cache,target=/root/.cache/uv \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    uv sync
+
+ENV PYTHONPATH=/app
+
+COPY ./maga_wish /app/maga_wish
+
+CMD ["uvicorn", "maga_wish.shared.infra.http.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
---
+++ maga_wish/shared/infra/http/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+
+from maga_wish.shared.infra.routes.main import api_router 
+from maga_wish.shared.environment.main import settings
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    # openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
