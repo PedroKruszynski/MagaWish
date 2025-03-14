b6ca8bbfa900bc04d8b9794715975dd5828921ca
PedroKruszynski <pedrokruszynski@gmail.com>
2025-03-14 02:11:24 -0300

    feat: add swagger file

--- maga_wish/shared/infra/http/main.py
+++ maga_wish/shared/infra/http/main.py
@@ -1,17 +1,20 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
-from contextlib import asynccontextmanager
-import logging
 
-from maga_wish.shared.infra.routes.main import api_router 
 from maga_wish.shared.environment.main import settings
 from maga_wish.shared.infra.redis.main import RedisDefault
+from maga_wish.shared.infra.routes.main import api_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,10 +28,11 @@ async def lifespan(app: FastAPI):
     logger.info("Closing Redis connection")
     await redisInstance.shutdown()
 
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     lifespan=lifespan,
-    # openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
 
